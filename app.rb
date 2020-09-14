25a5b0095c44bdb05ada711d6811adb6ce7772bc
Will-Gon <will.colocho@gmail.com>
2020-09-14 10:45:22 -0700

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Will"
+    end
+
+    get '/hometown' do 
+        "My hometown is Redwood city"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Baby Shark!"
+    end
 end
